24c4106f9d06cce63277201f690199c46797f810
chawol <chawolbaka@hotmail.com>
2018-08-10 21:43:04 +0800

    Init

---
+++ DisconnectGuard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DisconnectGuard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new NotImplementedException("纯脑洞,具体实现不存在的。");
+        }
+    }
+}
